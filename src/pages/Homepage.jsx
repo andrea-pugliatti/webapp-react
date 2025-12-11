e564ce53d357bc474ac4e71bf083fa6fef9e19dd
andrea-pugliatti <andrea.pugliatti@outlook.it>
2025-12-11 13:00:14 +0100

    Add Link for MovieDetail in Homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Homepage() {
 	const [movies, setMovies] = useState([]);
@@ -20,16 +21,18 @@ export default function Homepage() {
 			<div className="row">
 				{movies.map((movie) => (
 					<div key={movie.id} className="card">
-						<img
-							height={200}
-							src={`http://localhost:3000/img/${movie.image}`}
-							alt={movie.title}
-						/>
-						<div>{movie.title}</div>
-						<div>{movie.director}</div>
-						<div>{movie.genre}</div>
-						<div>{movie.release_year}</div>
-						<div>{movie.abstract}</div>
+						<Link to={`/movie/${movie.id}`}>
+							<img
+								height={200}
+								src={`http://localhost:3000/img/${movie.image}`}
+								alt={movie.title}
+							/>
+							<div>{movie.title}</div>
+							<div>{movie.director}</div>
+							<div>{movie.genre}</div>
+							<div>{movie.release_year}</div>
+							<div>{movie.abstract}</div>
+						</Link>
 					</div>
 				))}
 			</div>
