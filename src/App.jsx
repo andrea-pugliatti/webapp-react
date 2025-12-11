497db94000c661233d617949457cffb1571a21ff
andrea-pugliatti <andrea.pugliatti@outlook.it>
2025-12-11 12:00:32 +0100

    Add MovieDetail page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import DefaultLayout from "./layouts/DefaultLayout";
 import Homepage from "./pages/Homepage";
+import MovieDetail from "./pages/MovieDetail";
 
 function App() {
 	return (
@@ -8,6 +9,7 @@ function App() {
 			<Routes>
 				<Route Component={DefaultLayout}>
 					<Route path="/" element={<Homepage />} />
+					<Route path="/movie/:id" element={<MovieDetail />} />
 				</Route>
 			</Routes>
 		</BrowserRouter>
---
+++ src/pages/MovieDetail.jsx
@@ -0,0 +1,13 @@
+export default function MovieDetail() {
+	return (
+		<>
+			<h1>Movie</h1>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+				facilis reprehenderit accusamus earum id quia eligendi rem harum nemo!
+				Deserunt vel expedita excepturi maxime totam exercitationem doloribus
+				aut ipsam modi?
+			</p>
+		</>
+	);
+}
