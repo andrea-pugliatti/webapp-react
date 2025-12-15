8cb5e0c718817f3772fbe1fa2689df8454a9d925
andrea-pugliatti <andrea.pugliatti@outlook.it>
2025-12-15 15:34:43 +0100

    Add styles for the form

--- src/components/MovieReviewForm.jsx
+++ src/components/MovieReviewForm.jsx
@@ -30,43 +30,44 @@ export default function MovieReviewForm({ movieId }) {
 	};
 
 	return (
-		<form onSubmit={handleSubmit}>
-			<div className="form-name">
-				<label htmlFor="name">Name</label>
-				<input
-					type="text"
-					name="name"
-					id="name"
-					placeholder="John"
-					value={formData.name}
-					onChange={(e) => setFormData({ ...formData, name: e.target.value })}
-					required
-				/>
-			</div>
+		<form onSubmit={handleSubmit} className="review-form">
+			<div className="form-top">
+				<div className="form-name">
+					<label htmlFor="name">Name</label>
+					<input
+						type="text"
+						name="name"
+						id="name"
+						placeholder="John"
+						value={formData.name}
+						onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+						required
+					/>
+				</div>
 
-			<div className="form-rating">
-				<label htmlFor="vote">Rating</label>
-				<select
-					name="vote"
-					id="vote"
-					value={formData.vote}
-					onChange={(e) => setFormData({ ...formData, vote: e.target.value })}
-					required
-				>
-					<option value={1}>1</option>
-					<option value={2}>2</option>
-					<option value={3}>3</option>
-					<option value={4}>4</option>
-					<option value={5}>5</option>
-				</select>
+				<div className="form-rating">
+					<label htmlFor="vote">Rating</label>
+					<select
+						name="vote"
+						id="vote"
+						value={formData.vote}
+						onChange={(e) => setFormData({ ...formData, vote: e.target.value })}
+						required
+					>
+						<option value={1}>1</option>
+						<option value={2}>2</option>
+						<option value={3}>3</option>
+						<option value={4}>4</option>
+						<option value={5}>5</option>
+					</select>
+				</div>
 			</div>
 
 			<div className="form-text">
-				<label htmlFor="text">Review</label>
 				<textarea
 					name="text"
 					id="text"
-					rows="3"
+					rows="5"
 					placeholder="Say something about this movie..."
 					value={formData.text}
 					onChange={(e) => setFormData({ ...formData, text: e.target.value })}
