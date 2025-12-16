5158eff1fa4a3065f398c927aeaad73523b43644
andrea-pugliatti <andrea.pugliatti@outlook.it>
2025-12-16 12:52:11 +0100

    Change styles for Loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,6 +3,6 @@ import "ldrs/react/Helix.css";
 
 export default function Loader() {
 	return <div className="loader">
-    <Helix size="100" speed="3.5" color="white" />;
+    <Helix size="100" speed="0.5" color="white" />;
   </div>
 }
