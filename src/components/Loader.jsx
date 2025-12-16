7c0b907f5aa9332f6bc973e9eb3f8536fdfa0747
andrea-pugliatti <andrea.pugliatti@outlook.it>
2025-12-16 21:04:16 +0100

    Remove semicolon from loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,7 +2,9 @@ import { Helix } from "ldrs/react";
 import "ldrs/react/Helix.css";
 
 export default function Loader() {
-	return <div className="loader">
-    <Helix size="100" speed="0.5" color="white" />;
-  </div>
+	return (
+		<div className="loader">
+			<Helix size="100" speed="0.5" color="white" />
+		</div>
+	);
 }
